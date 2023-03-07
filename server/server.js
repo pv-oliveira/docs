dc2c06fa9b80e38876e42cab4fbe8726e45992a3
Paulo Victor <paulo.victor.ig@hotmail.com>
2023-03-07 13:23:03 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+
+mongoose.connect('mongodb://127.0.0.1/google_docs').then(() => console.log('conect'));
+
+const io = require("socket.io")(3002, {
+  cors: {
+    origin: "http://127.0.0.1:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+const defaultValue = ""
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+      console.log(delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+  console.log("connected");
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
